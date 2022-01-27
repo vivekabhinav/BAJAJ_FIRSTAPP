40be04c6d80d81504d1339679e59314a09f6054e
vivekabhinav <vivekabhinav7@gmail.com>
2022-01-27 12:31:50 +0530

    PASS DATA BETWEEN ACTIVITY

--- app/src/main/java/com/example/bajaj_firstapp/HomeActicity.kt
+++ app/src/main/java/com/example/bajaj_firstapp/HomeActicity.kt
@@ -3,17 +3,24 @@ package com.example.bajaj_firstapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class HomeActicity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        var name = intent.extras?.getString("sk")
+        var tvHome = findViewById<TextView>(R.id.tvHome)
+        tvHome.text = name
     }
 
     fun handleClick(view: android.view.View) {
-        var intent: Intent    //kotlin says you can't have a variable as null
-        intent = Intent(this,MainActivity::class.java)  //no need to give the word new
+        var intent: Intent
+        intent = Intent(this,MainActivity::class.java)
+        intent.putExtra("AT","Abhinav Tripathi")
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/bajaj_firstapp/MainActivity.java
+++ app/src/main/java/com/example/bajaj_firstapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String name = getIntent().getExtras().getString("ds");
+        TextView mTextView = findViewById(R.id.tvMain);
+        mTextView.setText(name);
     }
 
     public void clickHandler(View view) {
         Toast.makeText(this, "loggin in", Toast.LENGTH_SHORT).show();
         Intent mIntent;
         mIntent  = new Intent(MainActivity.this,HomeActicity.class);
+
+        mIntent.putExtra("VT","Vivek Tripathi");
         startActivity(mIntent);
+
     }
 }
+
+
